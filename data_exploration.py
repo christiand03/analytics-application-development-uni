113e96a45d34177894e41160b07a180b1e488842
Meisterhaendler <paul.harmuth@googlemail.com>
2025-11-05 13:53:33 +0100

    Notebook

--- data_exploration.py
+++ data_exploration.py
@@ -2,8 +2,8 @@ import pandas as pd
 import streamlit as st
 import time
 start_time = time.time()
-df = pd.read_parquet("C:/Uni/Analytics Application Development/Projekt/Auftragsdaten")
-df2 = pd.read_parquet("C:/Uni/Analytics Application Development/Projekt/Positionsdaten")
+df = pd.read_parquet("C:/Users/paulh/Desktop/projects/AAD/Aufragsdaten.parquet")
+df2 = pd.read_parquet("C:/Users/paulh/Desktop/projects/AAD/Positionsdaten.parquet")
 load_time = time.time()
 loading_time = load_time - start_time
 st.write(f"Daten geladen. Es hat {loading_time:.4f} Sekunden gedauert.")
---
+++ table_desc.md
@@ -0,0 +1,40 @@
+## Auftragsdaten Table
+
+# Auftrags_ID
+32 verschiedene Tuple nach der 13 Stelle
+
+# Schadensnummer
+
+# KvaRechnung_ID
+
+# KvaRechnung_Nummer
+
+# Forderung_Netto
+
+# Empfehlung_Netto
+
+# Einigung_Netto
+
+# DH_ID
+
+# Land
+
+# PLZ_SO
+
+# PLZ_HW
+
+# PLZ_VN
+
+# address1_postalcose
+
+# Schadenart_Name
+
+# Falltyp_Name
+
+# Gewerk_Name
+
+# Kundengruppe
+
+# Differenz_vor_Zeitwert_Netto 
+
+# Handwerker_Name
