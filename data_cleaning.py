a3d4dd840b0d62330f3169636b833564517e4af0
Christian <christian.debbertin@gmail.com>
2025-11-05 23:43:26 +0100

    Add data cleaning script

---
+++ data_cleaning.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_parquet("resources/Auftragsdaten")
+df2 = pd.read_parquet("resources/Positionsdaten")
+
+print(f"Memory usage before converting:")
+df.info(memory_usage='deep')
+df2.info(memory_usage='deep')
+
+
+None_placeholder = ["-", "(leer)", "(null)", "wird vergeben", "unbekannter HW"]
+for placeholder in None_placeholder:
+    df = df.replace(placeholder, pd.NA)
+    df2 = df2.replace(placeholder, pd.NA)
+
+
+
+# Converting Object Types for df
+columns_to_convert = ['Land', 'PLZ_SO', 'PLZ_HW', 'PLZ_VN', 'address1_postalcode', 
+                       'Schadenart_Name', 'Falltyp_Name', 'Gewerk_Name', 'Kundengruppe', 
+                       'Handwerker_Name']
+
+# Convert Column with low Cardinality to Category
+df[columns_to_convert] = df[columns_to_convert].astype('category')
+
+# Convert the other Object Columns to String
+object_columns = df.select_dtypes('object').columns
+df[object_columns] = df[object_columns].astype('string')
+
+
+# Downcast integer columns
+int_cols = df.select_dtypes(include=['int64']).columns
+for col in int_cols:
+    df[col] = pd.to_numeric(df[col], downcast='integer')
+
+# Downcast float columns
+df_below_four_decimals = df.select_dtypes(include='float') \
+                                    .apply(lambda col: np.isclose(col, col.round(4))) \
+                                    .any() \
+                                    .loc[lambda s: s] \
+                                    .index.tolist()
+
+df[df_below_four_decimals] = df[df_below_four_decimals].astype('float32')
+
+# Converting Object Types for df2
+df2_columns_to_convert = ['KvaRechnung_ID', 'KvaRechnung_Nummer', 'Mengeneinheit', 'Bemerkung']
+
+# Converting to Category
+df2[df2_columns_to_convert] = df2[df2_columns_to_convert].astype('category')
+
+# Converting the rest to strings
+object_columns = df2.select_dtypes('object').columns
+df2[object_columns] = df2[object_columns].astype('string')
+
+# Downcast float columns
+df2_below_four_decimals = df2.select_dtypes(include='float') \
+                                    .apply(lambda col: np.isclose(col, col.round(4))) \
+                                    .any() \
+                                    .loc[lambda s: s] \
+                                    .index.tolist()
+
+df2[df2_below_four_decimals] = df2[df2_below_four_decimals].astype('float32')
+
+print(f"Memory usage after converting:")
+df.info(memory_usage='deep')
+df2.info(memory_usage='deep')
+
+
+df.to_parquet("resources/Auftragsdaten_konvertiert")
+df2.to_parquet("resources/Positionsdaten_konvertiert")
