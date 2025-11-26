93747db75587bafa992f361a0ec7d4ba353afe2f
Christian <christian.debbertin@gmail.com>
2025-11-26 12:56:15 +0100

    Added new metrics

--- metrics.py
+++ metrics.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import numpy as np
 
+print("Loading Data...")
 df = pd.read_parquet("resources/Auftragsdaten_konvertiert")
 #df2 = pd.read_parquet("resources/Positionsdaten_konvertiert")
+print("Data loaded.")
 
-
-def ratio_null_values(input_df):
+def ratio_null_values_column(input_df):
     length_df = len(input_df)
     ratio_dict = {}
     for column in input_df.columns:
@@ -13,6 +15,21 @@ def ratio_null_values(input_df):
         ratio_dict[column] = ratio_null
     
     return ratio_dict
+
+def ratio_null_values_rows(input_df, relevant_columns=None):
+    if relevant_columns is None:
+        df_to_check = input_df
+    else:
+        df_to_check = input_df[relevant_columns]
+
+    total_rows = len(df_to_check)
+    if total_rows == 0:
+        return 0.0
+
+    null_rows = df_to_check.isnull().any(axis=1).sum()
+    row_ratio = (null_rows / total_rows) * 100
+
+    return row_ratio
 
 def Kundengruppe_containing_test(df):
     anzahl_test = len(df[df['Kundengruppe'].str.contains('test', case=False, na=False)])
@@ -56,4 +73,80 @@ def plausibilitaetscheck_forderung_einigung(input_df):
     
     return count, avg
 
-print(plausibilitaetscheck_forderung_einigung(df))
+def uniqueness_check(df, df2):
+    kvarechnung_id_is_unique = df['KvaRechnung_ID'].is_unique
+    position_id_is_unique = df2['Position_ID'].is_unique
+    
+    return kvarechnung_id_is_unique, position_id_is_unique
+
+# Metric to display number of rows after applying filters
+def count_rows(input_df):
+    count = len(input_df)
+    return count
+
+def split_dataframe(input_df, chunks=5):
+    return np.array_split(input_df, chunks)
+
+def data_cleanliness(input_df):
+    # needs to be set by Frontend
+    group_by_col = "Kundengruppe"
+
+    if group_by_col is None:
+        null_ratio_rows = ratio_null_values_rows(input_df)
+        null_ratio_cols = ratio_null_values_column(input_df)
+
+        return null_ratio_rows, null_ratio_cols
+
+    else:
+        # Group for columns & rows
+        grouped_null_counts = input_df.groupby(group_by_col).apply(lambda x: x.isnull().sum())
+        grouped_null_rows = input_df.groupby(group_by_col).apply(lambda x: x.isnull().any(axis=1).sum())
+        
+        # create group sizes
+        group_sizes = input_df.groupby(group_by_col).size()
+        
+        # calculate ratios
+        grouped_col_ratios = grouped_null_counts.div(group_sizes, axis=0)
+        grouped_row_ratios = grouped_null_rows / group_sizes
+
+        return grouped_col_ratios, grouped_row_ratios
+
+def groupby_col(input_df, col):
+    input_df_grouped = input_df.groupby(col)
+
+    return input_df_grouped
+
+def discount_check(df):
+    potential_errors = (~df['plausibel']).sum()
+    return potential_errors
+
+def proformabelege(df):
+    proforma = df[df['Einigung_Netto'].between(0.01, 1)]
+    proforma_count = len(proforma)
+    return proforma, proforma_count
+
+# das muss noch besser gemacht werden
+def position_count(input_df):
+    position_count = input_df.groupby('KvaRechnung_ID')['Position_ID'].count().reset_index()
+    print(type(position_count))
+    return position_count
+
+def einigung_negativ(df):
+    einigung_negative = df['Einigung_Netto'] < 0
+    all_negative = (df['Forderung_Netto'] < 0) & (df['Empfehlung_Netto'] < 0)
+    false_data = df[einigung_negative & ~all_negative]
+    error_count = len(false_data)
+
+    return error_count
+
+def above_50k(df):
+    suspicious_data = df[df['Einigung_Netto'] >= 50000]
+    return suspicious_data
+
+# def check_zeitwert(df):
+#     for index, row in df.iterrows():
+#         if row['Forderung_Netto'] - row['Einigung_Netto'] != 0:
+            
+
+
+print(einigung_negativ(df))
