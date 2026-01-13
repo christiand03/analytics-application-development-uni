c97d482ec18d8c4bb020a3735b2d548bedcf4218
cornelius eing <ceing@example.com>
2026-01-13 17:23:55 +0100

    data drift page5 (naive/no-db)

--- app_pages/page5.py
+++ app_pages/page5.py
@@ -1,5 +1,102 @@
 import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import data_drift_metrics as ddm
+
+# TODO:Alternative für duckdb schreiben, mit fetch nur bei bedarf
+def show_page(df, df2, metrics_df1, metrics_df2, metrics_combined):
+    st.title("Data Drift Reports")
+    min_date = df["CRMEingangszeit"].min().date()
+    max_date = df["CRMEingangszeit"].max().date()
+    min_date_6m = df["CRMEingangszeit"].min().date() + pd.Timedelta(weeks=26)
+    min_date_12m = df["CRMEingangszeit"].min().date() + pd.Timedelta(weeks=52)
+    report_html= None
+    path_to_report = None
+    #Date-Picker, für Referenzset und Evalset, Quelldatenset
+    with st.expander("Reportauswahl"):
+        with st.form("Vergleichszeiträume"):
+        
+            source_designation = st.selectbox(
+                "Datenquelle",
+                ("Auftragsdaten", "Positionsdaten"),
+                )
+            
+            col1, col2 = st.columns(2)
+            with col1:
+                start_date_reference, end_date_reference = st.date_input(
+                    "Referenzrahmen",
+                    (min_date,min_date_6m),
+                    min_date,max_date                                   
+                    )
+            with col2:    
+                start_date_eval, end_date_eval = st.date_input(
+                    "Vergleichsrahmen",
+                    (min_date_6m,min_date_12m),
+                    min_date,max_date
+                    )
+                
+            force_reload = st.checkbox("Refresh erzwingen?")
+
+            submitted = st.form_submit_button("Report anzeigen")
+
+            if submitted:
+            #Check für den Ordnerpfad
+                st.write("Pfad zum Report:")
+                if source_designation == "Auftragsdaten":
+                        source_df = df.dropna(subset=['CRMEingangszeit'])
+                        path_to_report = f"resources/reports/eval_df_{str(start_date_reference)}_{str(end_date_reference)}_{str(start_date_eval)}_{str(end_date_eval)}.html"
+                        st.code("./"+path_to_report)
+                if source_designation == "Positionsdaten":
+                        source_df = df2.dropna(subset=['CRMEingangszeit'])
+                        path_to_report = ("resources/reports/eval_df2_"+
+                                                        str(start_date_reference)+"_"+
+                                                        str(end_date_reference)+"_"+
+                                                        str(start_date_eval)+"_"+
+                                                        str(end_date_eval)+
+                                                        ".html") 
+                        st.code("./"+path_to_report)
+                if force_reload:
+                        st.write("-> Report wird neu erstellt <-")
+
+    #prevents use of uninitialized values further down the line    
+    if not path_to_report:
+         st.write("Warten auf Auswahl..")
+         st.stop()
+
+         
+    try: #versuche den gewünschten report zu finden
+        if not force_reload:
+            report_html = open(path_to_report, "r", encoding="utf-8").read()
+            components.html(report_html, height= 1000, scrolling=True)
+        else:
+             raise FileNotFoundError    
+    except FileNotFoundError: #erstelle fehlenden report, dazu feedback
+        with st.empty():
+            st.write("Report ist noch nicht vorhanden und wird erstellt. Dies kann einige Momente dauern...")
+            ddm.data_drift_evaluation(
+                source_df, 
+                start_date_reference, end_date_reference,
+                start_date_eval, end_date_eval
+                )
+            st.write("Report erstellt.")
+        report_html = open(path_to_report, "r", encoding="utf-8").read()    
+        components.html(report_html, height= 1000, scrolling=True)
+    
+
+    
+    
 
 
-def show_page():
-    st.title("Page 5")
+
+
+
+
+
+
+
+
+
+
+    
+
+
---
+++ data_drift_metrics.py
@@ -0,0 +1,131 @@
+import pandas as pd
+import numpy as np
+from sklearn import datasets
+from evidently import Dataset
+from evidently import DataDefinition
+from evidently import Report
+from evidently.presets import DataDriftPreset, DataSummaryPreset
+
+# Schema für Auftragsdatenset, nur aufgeführte Spalten werden geprüft   
+schema_df = DataDefinition(
+    numerical_columns= ["Forderung_Netto", "Empfehlung_Netto", "Einigung_Netto", "Differenz_vor_Zeitwert_Netto"],
+    categorical_columns= ["Land","Schadenart_Name", "Falltyp_Name", "Gewerk_Name"],
+    id_column= "AuftragID",
+    timestamp= "CRMEingangszeit"
+) 
+#Analog für Positionsdaten
+schema_df2 = DataDefinition(
+    id_column= "Position_ID",
+    numerical_columns= ["Menge","Menge_Einigung", "EP", "EP_Einigung", "Forderung_Netto", "Einigung_Netto"],
+    timestamp="CRMEingangszeit"
+    )
+
+def check_start_end_date(start,end):
+    """Helper function. Checks if end follows start chronologically and reorders the two if needed. 
+
+    Parameters
+    ----------
+    start : datetime
+        The assumed beginnig of the interval.
+    end : datetime
+        The assumed end of the interval.
+
+    Returns
+    -------
+    datetime, datetime
+        Pair of chronologically sorted datetime values.
+    """
+    if(start > end):
+       start, end = end, start
+    return start, end
+
+
+def datetime_slice_mask(df, start_date, end_date):
+    """Helper function. Returns a chronologically sliced Dataset according to passed datetime.
+
+    Parameters
+    ----------
+    df : pandas.Dataframe
+        input df
+    start_date : date
+    
+    end_date : date
+      
+
+    Returns
+    -------
+    evidently.Dataset
+        sliced DataFrame, converted to Dataset.
+    """
+    #Type cleanup to full datetime, due to the dashboard passing only date-level precision values
+    start_date = pd.to_datetime(start_date).replace(hour=0,minute=0,second=0)
+    end_date = pd.to_datetime(end_date).replace(hour=0,minute=0,second=0) 
+
+    mask =(df["CRMEingangszeit"] >= start_date) & (df["CRMEingangszeit"] < end_date)
+
+    if 'Kundengruppe' in df.columns: #evaluates true if Auftragsdaten-df was passed 
+        sliced_ds = Dataset.from_pandas(
+            df.loc[mask],
+            data_definition=schema_df
+        ) 
+    if 'Menge' in df.columns: #evaluates true if Positionsdaten-df was passed
+        sliced_ds = Dataset.from_pandas(
+            df.loc[mask],
+            data_definition=schema_df2
+        )  
+    return sliced_ds
+
+
+def data_drift_evaluation(df, start_date_reference, end_date_reference, start_date_eval, end_date_eval):
+    """Uses the standard preset in the evidentlyai framework to evaluate data drift between two samples (chosen by time interval) from the passed DataFrame. The resulting Snapshot object is saved as html for easy embedding.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        DataFrame to sample from
+    start_date_reference : datetime
+        starting datetime of the reference, baseline dataset
+    end_date_reference : datetime
+        ending datetime of the reference, baseline dataset
+    start_date_eval : datetime
+        starting datetime of the evaluated dataset 
+    end_date_eval : datetime
+        starting datetime of the evaluated dataset
+    
+    """
+    #check if start and end dates are in chronologicl order, switch if needed
+    start_date_reference, end_date_reference = check_start_end_date(start_date_reference, end_date_reference)
+    start_date_eval, end_date_eval = check_start_end_date(start_date_eval, end_date_eval)
+    
+    #create sliced datasets for analysis (mask-based)
+    reference_data = datetime_slice_mask(df,start_date_reference,end_date_reference)
+    eval_data = datetime_slice_mask(df, start_date_eval,end_date_eval) 
+    
+    if 'Kundengruppe' in df.columns: #evaluates true if Auftragsdaten-df was passed
+        report = Report([
+            DataDriftPreset(
+                columns=["Forderung_Netto", "Empfehlung_Netto", "Einigung_Netto", "Differenz_vor_Zeitwert_Netto","Land","Schadenart_Name", "Falltyp_Name", "Gewerk_Name"]
+                )
+        ])
+        my_eval = report.run(eval_data, reference_data)
+        my_eval.save_html("resources/reports/eval_df_"+
+                          str(start_date_reference)+"_"+
+                          str(end_date_reference)+"_"+
+                          str(start_date_eval)+"_"+
+                          str(end_date_eval)+
+                          ".html")
+    if 'Menge' in df.columns: #evaluates true if Positionsdaten-df was passed
+        report = Report([
+            DataDriftPreset(
+                columns=["Menge","Menge_Einigung", "EP", "EP_Einigung", "Forderung_Netto", "Einigung_Netto"]
+                )
+        ])
+        my_eval = report.run(eval_data, reference_data)
+        my_eval.save_html("resources/reports/eval_df2_"+
+                          str(start_date_reference)+"_"+
+                          str(end_date_reference)+"_"+
+                          str(start_date_eval)+"_"+
+                          str(end_date_eval)+
+                          ".html")     
+
+
